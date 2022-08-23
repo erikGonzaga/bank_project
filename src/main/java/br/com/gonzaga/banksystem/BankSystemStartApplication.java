1bb9d9a3ddaadfcfc72e7302df81c1a33d09e5a8
erikgonzaga <erikgonzaga@outlook.com>
2022-08-23 18:08:28 -0300

    Testes de Implementação de Endereço

--- src/main/java/br/com/gonzaga/banksystem/BankSystemStartApplication.java
+++ src/main/java/br/com/gonzaga/banksystem/BankSystemStartApplication.java
@@ -9,16 +9,21 @@ import java.util.Scanner;
 public class BankSystemStartApplication {
 
 	public static void main(String[] args) {
-		int option = 0;
+		int option = 0;							// <- Variável de inicialização
 
 		while (option == 0){
-			option = showAndGetMenuOption();
+			option = showAndGetMenuOption();	/* <- Enquanto for zero, a opção será inválida,
+												logo abaixo no try, catch, numeros inseridos fora do intervalo,
+												1 a 9 retornaram 0, o que gera opção inválida. */
 			if (option == 0) {
 				System.out.println("Opção Inválida: Insira um numero de 1 a 9");
 			}
 
+			// ʋ Desenvolvimento do Menu:
+
 			switch (option){
 				case 1:
+					// Implementação de Serviço de Captura de Endereço
 					AddressService addressService = new AddressServiceImpl();
 					System.out.println("Informe seu Endereço: \n" +
 					"Rua Exemplo, 123, Cidade, UF, CEP, Complemento");
@@ -61,32 +66,36 @@ public class BankSystemStartApplication {
 		}
 	}
 
+	// ʋ Criando o Menu inicial.
 	private static int showAndGetMenuOption() {
-		System.out.println
-				("----------------------------------------------------------------\n" +
-						"\n" +
-						"BANK SYSTEM MENU \n" +
-						"----------------------------------------------------------------\n" +
-						"----------------------------------------------------------------");
-		System.out.println
-				("Selecione uma Opção:\n" +
-						"\n" +
-						"01 - Criar Conta Bancaria\n" +
-						"02 - Depositar\n" +
-						"03 - Sacar\n" +
-						"04 - Transferir\n" +
-						"05 - Saldo\n" +
-						"06 - Extrato\n" +
-						"07 - Ativar Conta\n" +
-						"08 - Desativar Conta\n" +
-						"09 - Sair" +
-						" \n" +
-						"v --------------------------------------------------------");
+		System.out.println 	("----------------------------------------------------------------\n" +
+							"\n" +
+							"BANK SYSTEM MENU \n" +
+							"----------------------------------------------------------------\n" +
+							"----------------------------------------------------------------");
+		System.out.println ("Selecione uma Opção:\n" +
+							"\n" +
+							"01 - Criar Conta Bancaria\n" +
+							"02 - Depositar\n" +
+							"03 - Sacar\n" +
+							"04 - Transferir\n" +
+							"05 - Saldo\n" +
+							"06 - Extrato\n" +
+							"07 - Ativar Conta\n" +
+							"08 - Desativar Conta\n" +
+							"09 - Sair" +
+							" \n" +
+							" ʋ --------------------------------------------------------------");
 
+		// Capturando Exception caso haja inserção de letras.
 		try {
-			int opt = new Scanner(System.in).nextInt();
+			int opt = new Scanner(System.in).nextInt();  	// <- Inicio da Operação através do Scanner.
+
+			/* ʋ Caso a inserção esteja entre o intervalo, retorne opt (o valor da inserção),
+			caso contrario retorne 0 */
 			return opt > 0 && opt <= 9 ? opt : 0;
 
+			// ʋ Retorna 0, caso valores sejam diferentes aos do Intervalo.
 		} catch (InputMismatchException e){
 			return 0;
 		}
--- src/main/java/services/impl/AddressServiceImpl.java
+++ src/main/java/services/impl/AddressServiceImpl.java
@@ -5,16 +5,52 @@ import services.AddressService;
 public class AddressServiceImpl implements AddressService {
 
     @Override
-    public void createAddress(String address) {
-
+    public void createAddress(String address) {           // <- Criando novo endereço
     }
 
     @Override
-    public boolean isAddressValid(String address) {
-        String[] token = address.split(",");
+    public boolean isAddressValid(String address) {       // <- Validando Endereço
 
-        if (token.length < 5 || token.length > 6) return false;
+        String[] token = address.split(",");        // <- Criando um Array e Separando endereço por virgula.
+        System.out.println(token.length);
 
-        return false;
+            String street = token[0].trim();
+            String number = token[1].trim();
+            String city = token[2].trim();
+            String state = token[3].trim();
+            String cep = token[4].trim();
+
+            if (token.length < 5 || token.length > 6)         // <- Validando formato do endereço
+                return false;                                 // (Valido quando 5 ou 6) campos estiverem preenchidos.
+
+            // Validando Rua (Address)
+            if (street.length() < 5 || street.length() > 30)
+                return false;
+
+            // Validando numero (Number)
+            if (!number.matches("\\d{1,5}"))
+                return false;
+
+            // Validando Cidade (City)
+            if (city.length() < 3 || city.length() > 30)
+                return false;
+
+            // Validando Estado (State)
+            if (state.length() != 2)
+                return false;
+
+            // Validando CEP (Postal Code)
+            if (cep.length() != 8)
+                return false;
+
+            // Validando Complemento (Address 2)
+            if (token.length == 6 && token[5].length() > 30)
+                return false;
+
+            return true;
+    }
+
+    private boolean streetValidator(String street){
+        return street == null || street.length() < 5 || street.length() > 30;
     }
 }
