7934c9062d9c75ba11a2f8b1159a8e8377ad7a53
erikgonzaga <erikgonzaga@outlook.com>
2022-08-30 01:16:24 -0300

    Conexão com o DB

---
+++ src/main/java/utils/AplicationProperties.java
@@ -0,0 +1,8 @@
+package utils;
+
+public class AplicationProperties {
+
+    public static final String DB_CONNECTION = "jdbc:mysql://localhost:3306/db_bank_system";
+    public static final String DB_USERNAME = "root";
+    public static final String DB_PASSWORD = "root";
+}
